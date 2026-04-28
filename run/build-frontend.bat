6179837396e3c3f3ddd6ebb98b265a38fa4a3f5d
brokhuli <sfullom@gmail.com>
2026-04-28 15:37:25 -0400

    updated build-frontend.bat to install dependencies

--- run/build-frontend.bat
+++ run/build-frontend.bat
@@ -8,4 +8,8 @@ call "%VSDEVCMD%" -arch=x64 -host_arch=x64
 if errorlevel 1 exit /b %ERRORLEVEL%
 
 "%CMAKE%" --build "%REPO%\build" --config Release --target injector-frontend
+if errorlevel 1 exit /b %ERRORLEVEL%
+
+set "WINDEPLOYQT=C:\Qt\6.7.3\msvc2019_64\bin\windeployqt.exe"
+"%WINDEPLOYQT%" --qmldir "%REPO%\frontend\qml" "%REPO%\build\frontend\Release\injector-frontend.exe"
 exit /b %ERRORLEVEL%
